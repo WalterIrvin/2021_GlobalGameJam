f9e34544feebf0e6bee65eb35a10e01d9fe9428d
WalterIrvin <walterdirvin@gmail.com>
2021-02-01 19:23:44 -0700

    Reset

--- The_Game/Scenes/health_bar.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-onready var health_bar = $HealthBar
-
-func on_health_updated(health,amount):
-	health_bar.value = health
-	
-func on_max_health_updated(max_health):
-	health_bar.max_value = max_health
--- The_Game/Scripts/Initialize Scene.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-export (PackedScene) var Player
-var p = null
-
-func _ready():
-	p = Player.instance()
-	add_child(p)
-	p.position = $PlayerSpawner.global_position
